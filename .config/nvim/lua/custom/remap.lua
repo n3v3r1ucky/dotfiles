93ba0a0f55b7592fe9e6bdb03f347b613d20847e
neverlucky <>
2025-01-30 00:06:19 +0300

    Fix go to next search result map

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -7,8 +7,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nznv")
-vim.keymap.set("n", "N", "Nznv")
+vim.keymap.set("n", "n", "nzn")
+vim.keymap.set("n", "N", "Nzn")
 
 vim.keymap.set("x", "<leader>p", "\"_dp")
 
