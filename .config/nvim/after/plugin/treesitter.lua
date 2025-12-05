ac5d228db02ece77c825c2c5f2e570af63b70f0f
neverlucky <neverlucky@example.com>
2025-12-05 17:45:51 +0300

    Fix formatting

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -24,3 +24,4 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -2,136 +2,136 @@
 -- Now with Lazy.nvim!
 
 return {
-  -- Package manager
-  {
-    "folke/lazy.nvim",
-  },
-
-  -- Telescope: fuzzy finder
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "BurntSushi/ripgrep",
+    -- Package manager
+    {
+        "folke/lazy.nvim",
     },
-  },
 
-  -- Nord color scheme
-  {
-    "shaunsingh/nord.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme nord]])
-    end,
-  },
-
-  -- Treesitter: better syntax highlighting
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-  },
-
-  -- Undo tree visualizer
-  {
-    "mbbill/undotree",
-  },
-
-  -- Git integration
-  {
-    "tpope/vim-fugitive",
-  },
-
-  -- Autocompletion
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-    },
-  },
-  -- Debug Adapter Protocol
-  {
-    "mfussenegger/nvim-dap",
-  },
-  {
-      "rcarriga/nvim-dap-ui",
-      dependencies = {
-          "mfussenegger/nvim-dap",
-          "nvim-neotest/nvim-nio",
-      }
-  },
-  -- Python debugging
-  {
-    "mfussenegger/nvim-dap-python",
-  },
-
-  -- Golang debugging
-  {
-    "leoluz/nvim-dap-go",
-  },
-
-  -- Harpoon: quick navigation
-  {
-    "ThePrimeagen/harpoon",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
-
-  -- Git signs
-  {
-    "lewis6991/gitsigns.nvim",
-  },
-
-  -- Neogen: documentation generator
-  {
-    "danymat/neogen",
-    config = function()
-      require("neogen").setup {
-        enabled = true,
-        languages = {
-          python = {
-            template = {
-              annotation_convention = "reST",
-            },
-          },
+    -- Telescope: fuzzy finder
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "BurntSushi/ripgrep",
         },
-      }
-    end,
-    -- Uncomment next line if you want to follow only stable versions
-    -- tag = "*"
-  },
-  {
-      "ThePrimeagen/refactoring.nvim",
-      dependencies = {
-          "nvim-lua/plenary.nvim",
-          "nvim-treesitter/nvim-treesitter",
-      },
-      lazy = false,
-      opts = {},
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
-    lazy = false, -- neo-tree will lazily load itself
-  },
-  {
-      "antosha417/nvim-lsp-file-operations",
-      dependencies = {
-          "nvim-lua/plenary.nvim",
-          -- Uncomment whichever supported plugin(s) you use
-          --"nvim-tree/nvim-tree.lua",
-          "nvim-neo-tree/neo-tree.nvim",
-          -- "simonmclean/triptych.nvim"
-      },
-      config = function()
-          require("lsp-file-operations").setup()
-      end,
-  },
+
+    -- Nord color scheme
+    {
+        "shaunsingh/nord.nvim",
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            vim.cmd([[colorscheme nord]])
+        end,
+    },
+
+    -- Treesitter: better syntax highlighting
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
+
+    -- Undo tree visualizer
+    {
+        "mbbill/undotree",
+    },
+
+    -- Git integration
+    {
+        "tpope/vim-fugitive",
+    },
+
+    -- Autocompletion
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+        },
+    },
+    -- Debug Adapter Protocol
+    {
+        "mfussenegger/nvim-dap",
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
+        }
+    },
+    -- Python debugging
+    {
+        "mfussenegger/nvim-dap-python",
+    },
+
+    -- Golang debugging
+    {
+        "leoluz/nvim-dap-go",
+    },
+
+    -- Harpoon: quick navigation
+    {
+        "ThePrimeagen/harpoon",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
+
+    -- Git signs
+    {
+        "lewis6991/gitsigns.nvim",
+    },
+
+    -- Neogen: documentation generator
+    {
+        "danymat/neogen",
+        config = function()
+            require("neogen").setup {
+                enabled = true,
+                languages = {
+                    python = {
+                        template = {
+                            annotation_convention = "reST",
+                        },
+                    },
+                },
+            }
+        end,
+        -- Uncomment next line if you want to follow only stable versions
+        -- tag = "*"
+    },
+    {
+        "ThePrimeagen/refactoring.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        lazy = false,
+        opts = {},
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons", -- optional, but recommended
+        },
+        lazy = false,                      -- neo-tree will lazily load itself
+    },
+    {
+        "antosha417/nvim-lsp-file-operations",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            -- Uncomment whichever supported plugin(s) you use
+            --"nvim-tree/nvim-tree.lua",
+            "nvim-neo-tree/neo-tree.nvim",
+            -- "simonmclean/triptych.nvim"
+        },
+        config = function()
+            require("lsp-file-operations").setup()
+        end,
+    },
 }
